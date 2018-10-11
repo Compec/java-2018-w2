c943465376fcbe940c02c33eb500e226d09e9cf7
Mehdi Saffar <saffar.mehdi3@gmail.com>
2018-10-11 21:15:01 +0300

    Added code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,66 +4,106 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
+    public static void starSquare() {
+        System.out.println("Star square example ----");
+        // Change the following variables to change the width and height of the drawing
+        int lineCount = 5; // We want 5 lines of...
+        int starPerLineCount = 5; // ... 5 stars
+        // => will print a 5x5 square of stars
+        for(int lineIndex = 1; lineIndex <= lineCount; lineIndex++) {
+            // This inner loop will print exactly 5 stars on this line
+            for(int starIndex = 1; starIndex <= starPerLineCount; starIndex++) {
+                // We use .print instead of .println because each star
+                // should be printed on the same line
+                System.out.print("*");
+            }
+            // We only go back to the line after we print all the stars of this line
+            System.out.println();
+        }
 
-    public static int giveBirthYear(int currentYear, int age) {
-        int birthYear = currentYear - age;
-        return birthYear;
+        System.out.println();
     }
     public static void main(String[] args) {
-//        System.out.println("Hello");
-//        System.out.println("What is your name?");
-//        Scanner scanner = new Scanner(System.in);
-//        String name = scanner.nextLine();
-//        System.out.println("Hello, " + name);
-//        System.out.println("And what is your age?");
-//        int age = scanner.nextInt();
-//        System.out.println(new Date().getYear());
-//        int birthYear = giveBirthYear(2018, age);
-//        System.out.println(name + " is " + age + " years old.");
-//        System.out.println("which means " + name + " is born in " + birthYear);
-//
-//        System.out.println("so... what is your GPA? ");
-//        double gpa = scanner.nextDouble();
-//        if(gpa >= 3.5) {
-//            System.out.println("Wow you are a smart student");
-//        } else if(gpa >= 2.0) {
-//            System.out.println("You are doing quite well");
-//        } else if(gpa >= 1.0){
-//            System.out.println("Try better :)");
-//        } else {
-//            System.out.println("Find another university bro");
-//        }
+        starSquare();
+        diminishingStars();
+        increasingStars();
+        cumulativeSum(1, 10);
+        cumulativeProduct(1, 10);
+    }
 
-//        for(int count = 1; count <= 8; count++) {
-//            for(int starCount = 9 - count; starCount >= 1; starCount--) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//
-//        for(int count = 1; count <= 8; count++) {
-//            for(int starCount = 1; starCount <= count; starCount++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
+    public static void cumulativeProduct(int startNumber, int endNumber) {
+        System.out.println("Cumulative product example ----");
         int product = 1;
-        for(int i = 1; i <= 100; i++) {
-            product = product * i;
+        for(int number = startNumber; number <= endNumber; number++) {
+            product *= number;
         }
-        System.out.println(sum);
-        System.out.println(1 + 2 + 3 + 4 + 5);
-        // 1, 1*
-        // 2, 2**
-        // 3, 3***
-        // ****
-        // *****
+        System.out.println("The cumulative product from " + startNumber + " to " + endNumber + " is " + product);
 
-        // *****
-        // *****
-        // *****
-        // *****
-        // *****
+        System.out.println();
+    }
+
+    public static void cumulativeSum(int startNumber, int endNumber) {
+        System.out.println("Cumulative sum example ----");
+        int sum = 0;
+        for(int number = startNumber; number <= endNumber; number++) {
+            sum += number;
+        }
+        System.out.println("The cumulative sum from " + startNumber + " to " + endNumber + " is " + sum);
+
+        System.out.println();
+    }
+
+    public static void increasingStars() {
+        System.out.println("Increasing stars example ----");
+        // Change the following variable to change the number of lines
+        int lineCount = 5; // We want 5 lines of stars...
+        for(int lineIndex = 1; lineIndex <= lineCount; lineIndex++) {
+            // Each line will print exactly `lineIndex` stars
+            // so for lineIndex == ...
+            // 1 -> 1
+            // 2 -> 2
+            // 3 -> 3
+            // 4 -> 4
+            // 5 -> 5
+            // etc ...
+            // You can see the pattern ;)
+            int starPerLineCount = lineIndex;
+            for(int starIndex = 1; starIndex <= starPerLineCount; starIndex++) {
+                System.out.print("*");
+            }
+            // Println to go back to the line after all the stars of the current line
+            // have been printed
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    public static void diminishingStars() {
+        System.out.println("Diminishing stars example ----");
+        // Change the following variables to change the width and height of the drawing
+        int lineCount = 5; // We want 5 lines of stars...
+        for(int lineIndex = 1; lineIndex <= lineCount; lineIndex++) {
+            // Each line will print exactly `lineCount - lineIndex + 1` stars
+            // let's consider lineCount == 5
+            // so for lineIndex == ...
+            // 1 -> 5
+            // 2 -> 4
+            // 3 -> 3
+            // 4 -> 2
+            // 5 -> 1
+            // etc ...
+            // You can see the pattern ;)
+            int starPerLineCount = lineCount - lineIndex + 1;
+            for(int starIndex = 1; starIndex <= starPerLineCount; starIndex++) {
+                System.out.print("*");
+            }
+            // Println to go back to the line after all the stars of the current line have been
+            // printed
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
+
