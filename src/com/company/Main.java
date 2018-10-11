dd3edb903d4087cab69f198ba4c8cfdf8c33130b
Mehdi Saffar <saffar.mehdi3@gmail.com>
2018-10-11 20:37:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int giveBirthYear(int currentYear, int age) {
+        int birthYear = currentYear - age;
+        return birthYear;
+    }
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        System.out.println("What is your name?");
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.nextLine();
+//        System.out.println("Hello, " + name);
+//        System.out.println("And what is your age?");
+//        int age = scanner.nextInt();
+//        System.out.println(new Date().getYear());
+//        int birthYear = giveBirthYear(2018, age);
+//        System.out.println(name + " is " + age + " years old.");
+//        System.out.println("which means " + name + " is born in " + birthYear);
+//
+//        System.out.println("so... what is your GPA? ");
+//        double gpa = scanner.nextDouble();
+//        if(gpa >= 3.5) {
+//            System.out.println("Wow you are a smart student");
+//        } else if(gpa >= 2.0) {
+//            System.out.println("You are doing quite well");
+//        } else if(gpa >= 1.0){
+//            System.out.println("Try better :)");
+//        } else {
+//            System.out.println("Find another university bro");
+//        }
+
+//        for(int count = 1; count <= 8; count++) {
+//            for(int starCount = 9 - count; starCount >= 1; starCount--) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//        for(int count = 1; count <= 8; count++) {
+//            for(int starCount = 1; starCount <= count; starCount++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        int product = 1;
+        for(int i = 1; i <= 100; i++) {
+            product = product * i;
+        }
+        System.out.println(sum);
+        System.out.println(1 + 2 + 3 + 4 + 5);
+        // 1, 1*
+        // 2, 2**
+        // 3, 3***
+        // ****
+        // *****
+
+        // *****
+        // *****
+        // *****
+        // *****
+        // *****
+    }
+}
